638f33bc03f693c160c1662dcb327cd3bce09113
Bertrand THOMAS <bertrand@devpro.fr>
2026-03-01 17:03:28 +0100

    Add library

--- dungeon-slime/src/DesktopApp/MainGame.cs
+++ dungeon-slime/src/DesktopApp/MainGame.cs
@@ -1,21 +1,11 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
+﻿using GameEngine;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 
 namespace DesktopApp;
 
-public class Game1 : Game
+public class MainGame() : GameBase("Dungeon Slime", 1280, 720, false)
 {
-    private GraphicsDeviceManager _graphics;
-    private SpriteBatch _spriteBatch;
-
-    public Game1()
-    {
-        _graphics = new GraphicsDeviceManager(this);
-        Content.RootDirectory = "Content";
-        IsMouseVisible = true;
-    }
-
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
@@ -25,15 +15,18 @@ public class Game1 : Game
 
     protected override void LoadContent()
     {
-        _spriteBatch = new SpriteBatch(GraphicsDevice);
-
         // TODO: use this.Content to load your game content here
+
+        base.LoadContent();
     }
 
     protected override void Update(GameTime gameTime)
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed
+            || Keyboard.GetState().IsKeyDown(Keys.Escape))
+        {
             Exit();
+        }
 
         // TODO: Add your update logic here
 
---
+++ dungeon-slime/src/GameEngine/GameBase.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameEngine;
+
+public class GameBase : Game
+{
+    internal static GameBase s_instance;
+
+    /// <summary>
+    /// Gets a reference to the Core instance.
+    /// </summary>
+    public static GameBase Instance => s_instance;
+
+    /// <summary>
+    /// Gets the graphics device manager to control the presentation of graphics.
+    /// </summary>
+    public static GraphicsDeviceManager Graphics { get; private set; }
+
+    /// <summary>
+    /// Gets the graphics device used to create graphical resources and perform primitive rendering.
+    /// </summary>
+    public static new GraphicsDevice GraphicsDevice { get; private set; }
+
+    /// <summary>
+    /// Gets the sprite batch used for all 2D rendering.
+    /// </summary>
+    public static SpriteBatch SpriteBatch { get; private set; }
+
+    /// <summary>
+    /// Gets the content manager used to load global assets.
+    /// </summary>
+    public static new ContentManager Content { get; private set; }
+
+    /// <summary>
+    /// Creates a new Game instance.
+    /// </summary>
+    /// <param name="title">The title to display in the title bar of the game window.</param>
+    /// <param name="width">The initial width, in pixels, of the game window.</param>
+    /// <param name="height">The initial height, in pixels, of the game window.</param>
+    /// <param name="fullScreen">Indicates if the game should start in fullscreen mode.</param>
+    protected GameBase(string title, int width, int height, bool fullScreen)
+    {
+        // ensures that multiple cores are not created
+        if (s_instance != null)
+        {
+            throw new InvalidOperationException($"Only a single Core instance can be created");
+        }
+
+        s_instance = this;
+
+        Graphics = new GraphicsDeviceManager(this);
+
+        Graphics.PreferredBackBufferWidth = width;
+        Graphics.PreferredBackBufferHeight = height;
+        Graphics.IsFullScreen = fullScreen;
+
+        Graphics.ApplyChanges();
+
+        Window.Title = title;
+
+        Content = base.Content;
+
+        Content.RootDirectory = "Content";
+
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+
+        GraphicsDevice = base.GraphicsDevice;
+
+        SpriteBatch = new SpriteBatch(GraphicsDevice);
+    }
+}
